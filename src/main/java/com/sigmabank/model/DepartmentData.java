fe53dd3ad3d483229c170e578bf0924e411d9384
AlexLukashevich95 <alex.lukashevich95@gmail.com>
2025-09-03 09:23:23 +0300

    feature(task-1): -added lombok

--- src/main/java/com/sigmabank/model/DepartmentData.java
+++ src/main/java/com/sigmabank/model/DepartmentData.java
@@ -1,30 +1,17 @@
 package com.sigmabank.model;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class DepartmentData {
     private Manager manager;
     private List<Employee> employees = new ArrayList<>();
 
     public DepartmentData(Manager manager) {
         this.manager = manager;
-    }
-
-    public Manager getManager() {
-        return manager;
-    }
-
-    public void setManager(Manager manager) {
-        this.manager = manager;
-    }
-
-    public List<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(List<Employee> employees) {
-        this.employees = employees;
     }
 }
 
--- src/main/java/com/sigmabank/model/StatData.java
+++ src/main/java/com/sigmabank/model/StatData.java
@@ -1,47 +1,13 @@
 package com.sigmabank.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class StatData {
     private String department;
     private String min;
     private String max;
     private String mid;
-
-    public StatData(String department, String min, String max, String mid) {
-        this.department = department;
-        this.min = min;
-        this.max = max;
-        this.mid = mid;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getMin() {
-        return min;
-    }
-
-    public void setMin(String min) {
-        this.min = min;
-    }
-
-    public String getMax() {
-        return max;
-    }
-
-    public void setMax(String max) {
-        this.max = max;
-    }
-
-    public String getMid() {
-        return mid;
-    }
-
-    public void setMid(String mid) {
-        this.mid = mid;
-    }
 }
